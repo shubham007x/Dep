965ef4268407283d6a62d3fafece5b7688e74167
shubham007x <shubham.agdari@gmail.com>
2023-10-14 00:04:06 +0530

    expenses

--- src/Redux/store.js
+++
@@ -1,6 +0,0 @@
-import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
-import { reducer } from "./reducer";
-import thunk from "redux-thunk";
-const rootReducer = combineReducers({ reducer });
-const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
-export { store };
